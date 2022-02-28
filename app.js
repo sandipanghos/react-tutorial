20dbdb8c845ca62542a8926e7a7392fa1e77cc0a
Sandipan Ghosh <sandipanghosh@Sandipans-MacBook-Pro.local>
2022-03-01 00:24:55 +0530

    likeButton component createdand add to html

---
+++ app.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { like: 0 };
+    this.onLike = this.onLike.bind(this);
+  }
+  onLike() {
+    this.setState((state, props) => ({ like: state.like + 1 }));
+  }
+  render() {
+    // Display a "Like" <button>
+    return e(
+      "button",
+      { onClick: () => this.onLike() },
+      `Like ${this.state.like}`
+    );
+  }
+}
+
+const domContainer = document.querySelector("#app");
+ReactDOM.render(e(LikeButton), domContainer);
