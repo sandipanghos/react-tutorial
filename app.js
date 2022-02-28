8edb1eb013c87d87741b906479f0ab30e26b9b05
sandipanghos <sandipanghosh64@gmail.com>
2022-03-01 01:45:28 +0530

    Merge branch 'master' of https://github.com/sandipanghos/react-tutorial into my-first-react-component

--- app.js
+++
@@ -1,8 +0,0 @@
-const e = React.createElement;
-
-function Greeting(props) {
-  return e("h1", {}, `Welcome ${props.username} !`);
-}
-
-const domContainer = document.querySelector("#app");
-ReactDOM.render(e(Greeting, { username: "Sandipan" }), domContainer);
