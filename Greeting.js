a23fd9acee9ac9aaffad87a63614b8b26e155164
sandipanghos <sandipanghosh64@gmail.com>
2022-03-03 10:31:56 +0530

    add-jsx-preprocessor

--- Greeting.js
+++
@@ -1,8 +0,0 @@
-var e = React.createElement;
-
-function Greeting(props) {
-  return React.createElement("h1", null, "Welcome ", props.username, " !");
-}
-
-var domContainer = document.querySelector("#greeting");
-ReactDOM.render(React.createElement(Greeting, { username: "John" }), domContainer);
